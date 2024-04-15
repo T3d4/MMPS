86626fda2533a8905e9a5b77dd12ac18bad726d5
T3d4 <tdansa64@gmail.com>
2024-04-15 15:34:32 +0100

    minor updates to auth routes

--- Backend/src/routes/auth_routes.ts
+++ Backend/src/routes/auth_routes.ts
@@ -1,1 +1,33 @@
 //Under development
+import { authenticateToken, isAdmin, validator } from "../middleware";
+import { AuthController } from "../controllers";
+import { Router } from "express";
+import {
+    createUserSchema,
+    loginSchema,
+} from "../validators";
+
+const {
+    createUser,
+    login,
+    refresh,
+    resetPassword,
+    logout,
+} = new AuthController();
+
+export const authRouter = Router();
+
+// admin creates each new user
+authRouter.route("/create").post(validator(createUserSchema), createUser);
+
+// admin or user logs in
+authRouter.route("/login").post(validator(loginSchema), login);
+
+// checks if the refresh token stored in cookie is valid and renews the accesstoken
+authRouter.route("/refresh").get(refresh);
+
+// // reset password after confirming otp
+// authRouter.route("/reset").patch(validator(resetPassSchema), resetPassword);
+
+// clears cookies and loggs out user
+authRouter.route("/logout").get(logout);
