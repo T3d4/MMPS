8d64f703c78d70126f9b48d60443d3b4746251d5
teda <tdansa64@gmail.com>
2024-05-14 07:31:20 +0100

    implemented components for quiz view

---
+++ Frontend/mmps/src/components/ResultComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const { numberOfCorrectAnswers, quizQuestionLength } = defineProps([
+  "quizQuestionLength",
+  "numberOfCorrectAnswers",
+]);
+</script>
+
+<template>
+  <div class="results">
+    <p>Your Result...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <RouterLink to="/">Go Back</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+
+p {
+  font-size: 25px;
+}
+
+h1 {
+  font-size: 80px;
+}
+
+a {
+  text-decoration: none;
+  text-decoration-color: aqua;
+  color: azure;
+}
+</style>
