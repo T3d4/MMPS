bd6e3443f1844afa3f22931e047ae0426588793b
teda <tdansa64@gmail.com>
2024-05-14 07:32:10 +0100

    quiz view

---
+++ Frontend/mmps/src/views/QuizView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import Question from "../components/QuestionComponent.vue";
+import QuizHeader from "../components/QuizHeaderComponent.vue";
+import Result from "../components/ResultComponent.vue";
+import { useRoute } from "vue-router";
+import { ref, computed } from "vue";
+import quizes from "../data/quizes.json";
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
+
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+// })
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
+</script>
+
+<template>
+  <div>
+    <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
+    <div>
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
+    </div>
+  </div>
+</template>
