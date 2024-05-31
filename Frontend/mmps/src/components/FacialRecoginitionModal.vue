5831e68214f5cf0890a9a6bdc60a45e5da070a8c
Teda <tdansa64@gmail.com>
2024-05-31 23:00:05 +0100

    minor bug fixes

--- Frontend/mmps/src/components/FacialRecoginitionModal.vue
+++ Frontend/mmps/src/components/FacialRecoginitionModal.vue
@@ -1,17 +1,17 @@
 <template>
-  <!-- facial recognition modal -->
   <div
-    v-if="props.show"
+    v-if="show"
     class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
   >
     <div class="bg-white w-1/3 rounded-lg p-8 shadow-md">
       <h2 class="text-2xl font-semibold mb-4 text-gray-700">Facial Recognition</h2>
 
       <video
-        id="video"
         ref="video"
         class="w-2/3 mx-auto rounded-lg border-[3px] border-indigo-500 shadow-md"
         autoplay
+        playsinline
+        muted
       ></video>
 
       <p v-if="!faceDetected && !faceAuthLoading" class="text-red-500 mt-2 pl-6">
@@ -25,9 +25,8 @@
         >
           Cancel
         </button>
-
         <button
-          @click="captureAndVerifyFace"
+          @click="capture"
           :disabled="!faceDetected || faceVerified || faceAuthLoading"
           :class="{
             'animate-pulse opacity-100 cursor-not-allowed': faceAuthLoading,
@@ -44,29 +43,26 @@
 </template>
 
 <script setup>
-import { ref, watch, onBeforeUnmount } from 'vue'
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue'
+import { capturing, showCamera } from '@/global_state/state'
 import * as faceapi from 'face-api.js'
-import router from '@/router'
-import * as axios from 'axios'
-import { capturing } from '@/global_state/state';
+import axios from 'axios'
 
 const props = defineProps({
-  show: Boolean, // Controls whether the modal is visible
-  quizId: String, // The ID of the quiz to take after verification
+  show: Boolean,
+  quizId: String
 })
 
-const emit = defineEmits(['close', 'verified', "notCaptured"])
+const emit = defineEmits(['close', 'verified', 'notCaptured'])
 
 const faceDetected = ref(false)
 const faceVerified = ref(false)
-const video = ref()
+const video = ref(null)
 const faceAuthLoading = ref(false)
 const errorMessage = ref(null)
-const showCamera = ref(false)
 const faceCaptured = ref(false)
 let faceCaptureInterval = null
 let faceCaptureTimeout = null
-
 
 const loadModels = async () => {
   await faceapi.nets.ssdMobilenetv1.loadFromUri('http://localhost:8900/models')
@@ -74,75 +70,15 @@ const loadModels = async () => {
   await faceapi.nets.faceRecognitionNet.loadFromUri('http://localhost:8900/models')
 }
 
-const startVideo = () => {
-  const constraints = (window.constraints = { audio: false, video: true })
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((stream) => {
-      if (video.value) {
-        video.value.srcObject = stream
-      }
-    })
-    .catch((error) => {
-      console.error('Error accessing camera:', error)
-    })
-}
-
-// Close the camera when navigating away from the page
-onBeforeUnmount(() => {
-  if (video.value && video.value.srcObject) {
-    const stream = video.value.srcObject
-    const tracks = stream.getTracks()
-    tracks.forEach((track) => track.stop())
+const startVideo = async () => {
+  const constraints = { audio: false, video: true }
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints)
+    video.value.srcObject = stream
+  } catch (error) {
+    console.error('Error accessing camera:', error)
   }
-})
-
-const captureAndVerifyFace = async() => {
-  faceCaptureInterval = setInterval(async () => {
-    try {
-      faceAuthLoading.value = true
-      const detection = await faceapi
-        .detectSingleFace(video.value)
-        .withFaceLandmarks()
-        .withFaceDescriptor()
-
-      if (detection) {
-        const faceDescriptor = Array.from(detection.descriptor)
-        const response = await axios.post(
-          '/api/v1/auth/validate-face', // Backend endpoint for face verification
-          { faceDescriptor, email: localStorage.getItem('user_email') } //send descriptor and email to be verified
-        )
-
-        if (response.data.success) {
-          // Face verification successful, start the quiz
-          faceVerified.value = true
-          showCamera.value = false
-          // Use router to navigate to the quiz page, passing the quizId
-          router.push({ name: 'Quiz', params: { id: props.quizId } })
-        } else {
-          errorMessage.value = 'Face not recognized. Please try again.'
-          // Handle verification failure (e.g., display an error message)
-        }
-      } else {
-        errorMessage.value = 'Face not detected. Please try again.'
-      }
-    } catch (error) {
-      console.error(error)
-      errorMessage.value = 'An error occurred during face verification.'
-    } finally {
-      faceAuthLoading.value = false
-    }
-  })
 }
-
-// Close the camera when navigating away from the page
-onBeforeUnmount(() => {
-  if (video.value && video.value.srcObject) {
-    const stream = video.value.srcObject
-    const tracks = stream.getTracks()
-    tracks.forEach((track) => track.stop())
-  }
-})
 
 const stopVideo = () => {
   if (video.value && video.value.srcObject) {
@@ -153,54 +89,90 @@ const stopVideo = () => {
   }
 }
 
+const captureAndVerifyFace = async () => {
+  faceCaptureInterval = setInterval(async () => {
+    try {
+      faceAuthLoading.value = true
+      const detection = await faceapi
+        .detectSingleFace(video.value)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
+
+      if (detection) {
+        const faceDescriptor = Array.from(detection.descriptor)
+        const response = await axios.post('/api/v1/auth/validate-face', {
+          faceDescriptor,
+          email: localStorage.getItem('user_email')
+        })
+
+        if (response.data.success) {
+          faceVerified.value = true
+          faceCaptured.value = true
+          showCamera.state = false
+          emit('verified')
+        } else {
+          errorMessage.value = 'Face not recognized. Please try again.'
+        }
+      } else {
+        errorMessage.value = 'Face not detected. Please try again.'
+      }
+    } catch (error) {
+      console.error(error)
+      errorMessage.value = 'An error occurred during face verification.'
+    } finally {
+      faceAuthLoading.value = false
+    }
+  }, 1000)
+}
 
 watch(showCamera, async (newShowCamera) => {
-  loadModels()
-  startVideo()
-
-  const checkFaceTimeout = () => {
-    if (faceCaptured.value) {
-      clearTimeout(faceCaptureTimeout)
-      showCamera.value = false
-      capturing.state = false
-      return true
-    }
-    return false
-  }
-
   if (newShowCamera) {
-    if (!loadModels()) {
-      await loadModels()
-    }
-
-    startVideo()
+    await loadModels()
+    await startVideo()
     captureAndVerifyFace()
 
     faceCaptureTimeout = setTimeout(() => {
-      if (!checkFaceTimeout()) {
+      if (!faceCaptured.value) {
         clearInterval(faceCaptureInterval)
-        showCamera.value = false
-        capturing.state = false
-        errorMessage.value = 'Failed to capture face... try again'
+        showCamera.state = false
+        errorMessage.value = 'Face capture timed out. Please try again.'
         stopVideo()
+        emit('notCaptured')
       }
     }, 15000)
-  } else if (checkFaceTimeout()) {
+  } else {
     clearInterval(faceCaptureInterval)
+    clearTimeout(faceCaptureTimeout)
+    stopVideo()
   }
 })
 
-function closeModal() {
-  emit('close')
+onMounted(() => {
+  if (props.show) {
+    showCamera.state = true
+  }
+})
+
+onBeforeUnmount(() => {
+  stopVideo()
+})
+
+const closeModal = () => {
+  showCamera.state = false
   capturing.state = false
+  clearInterval(faceCaptureInterval)
+  clearTimeout(faceCaptureTimeout)
+  stopVideo()
+  emit('close')
 }
 
-// Face detection watch (similar to previous code)
+// const capture = () => (showCamera.state = true)
+
 watch(
   () => faceapi.detectSingleFace(video.value).withFaceLandmarks().withFaceDescriptor(),
   (detections) => {
     faceDetected.value = !!detections
   },
-  { immediate: true } // Run the watcher immediately on mount
+  { immediate: true }
 )
 </script>
