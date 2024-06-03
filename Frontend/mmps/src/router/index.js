636c452786f99f06b0497e36a5a41da1d9c24a8e
Teda <tdansa64@gmail.com>
2024-06-03 14:07:44 +0100

    updated dependencies for admin dashboard

--- Frontend/mmps/src/router/index.js
+++ Frontend/mmps/src/router/index.js
@@ -4,6 +4,10 @@ import QuizView from '@/views/QuizView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
 import QuizesView from '@/views/QuizzesView.vue'
+import AdminDashboard from '@/views/AdminDashboardView.vue'
+import UserManagement from '@/views/UserManagementView.vue'
+import QuizManagement from '@/views/QuizManagementView.vue'
+import Statistics from '@/views/StatisticsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +38,26 @@ const router = createRouter({
       name: 'quiz',
       component: QuizView
 
+    },
+    {
+      path: '/admin',
+      name: 'AdminDashboard',
+      component: AdminDashboard
+    },
+    {
+      path: '/admin/user-management',
+      name: 'UserManagement',
+      component: UserManagement
+    },
+    {
+      path: '/admin/quiz-management',
+      name: 'QuizManagement',
+      component: QuizManagement
+    },
+    {
+      path: '/admin/statistics',
+      name: 'Statistics',
+      component: Statistics
     }
     //   path: '/about',
     //   name: 'about',
